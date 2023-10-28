bec9be05815b0d8b46ae197419299327bc24a109
IASV <ivanantoniosuarezvillalba@gmail.com>
2023-10-28 14:12:36 -0500

    change connections database

--- app/config/database.py
+++ app/config/database.py
@@ -1,12 +1,11 @@
 import MySQLdb
 
-# Database configuration
-db_config = {
-    "host": "DB",
-    "user": "admin",
-    "password": "admin",
-    "db": "mydatabase",
-}
-
-# Create a connection to the database
-conn = MySQLdb.connect(**db_config)
+# Create the connection object
+connection = MySQLdb.connect(
+    host="db",
+    user="admin",
+    password="admin",
+    db="mydatabase",
+)
+# Create cursor
+cursor = connection.cursor()
